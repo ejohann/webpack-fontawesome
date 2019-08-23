25645e74966947deddf655d73267f07273603a59
Johanne <johanne_lewis@hotmail.com>
2019-08-23 19:11:15 +0100

    Import icon from fontawesome library

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import HelloWorldButton from './components/hello-world-button/hello-world-button';
 import Heading from './components/heading/heading.js';
 import addImage from './add-image';
 import 'bootstrap';
 import './index.scss';
+
+
+library.add(faSpinner);
 
 const heading = new Heading();
 const helloWorldButton = new HelloWorldButton();
